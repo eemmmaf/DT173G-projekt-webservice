aadcd0b958a00679b4e2cc4a3d6a5e10acdeeab6
Emma Forslund <emfo2102@student.miun.se>
2022-05-31 11:28:04 +0200

    Skapat en klass som hanterar användare

---
+++ classes/User.class.php
@@ -0,0 +1,124 @@
+<?php
+class User
+{
+    private $username;
+    private $password;
+    private $db;
+    private $token;
+
+    //Konstruktor med databasanslutning
+    function __construct()
+    {
+        $this->db = new mysqli(DBHOST, DBUSER, DBPASS, DBDATABASE);
+
+        //Kontrollerar om det finns något fel
+        if ($this->db->connect_errno > 0) {
+            die("Fel vid anslutning" . $this->db->connect_error);
+        }
+    }
+
+    //Funktion för att generera en token vid inloggning
+    public function createToken(string $username): string
+    {
+        $token = "";
+        $characters = array_merge(range('A', 'Z'), range('a', 'z'), range('0', '9'));
+        $max = count($characters) - 1;
+        for ($i = 0; $i < 32; $i++) {
+            $generating = mt_rand(0, $max);
+            $token .= $characters[$generating];
+        }
+
+        $sql = "UPDATE user SET user_token='$token' WHERE username='$username'";
+
+        $result = $this->db->query($sql);
+
+        return $result;
+    }
+
+    //Funktion för att se om token finns i databasen
+    public function validateToken(string $token)
+    {
+        //SQL-fråga
+        $sql = "SELECT * FROM user WHERE token='$token'";
+
+        $result = $this->db->query($sql);
+
+        return $result;
+    }
+
+    //Metod för att ta bort token när användaren loggar ut
+    public function deleteToken($username)
+    {
+
+        $sql = "UPDATE user set user_token=NULL WHERE username='$username'";
+
+
+        $result = $this->db->query($sql);
+
+        return $result;
+    }
+
+
+    //Setmetod för att kolla så att inte fältet är tomt
+    public function setUser(string $username, string $password): bool
+    {
+        if ($username && $password != "") {
+            $this->username = $username;
+            $this->password = $password;
+
+            $username = $this->db->real_escape_string($username);
+            $password = $this->db->real_escape_string($password);
+
+            $username = strip_tags($username);
+            $password = strip_tags($password);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setToken(string $token)
+    {
+        if ($token != "") {
+            $this->token = $token;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+    //Metod för att logga in 
+    public function logIn(string $username, string $password): bool
+    {
+
+        //Kontrollerar om set-metoder är uppfyllda
+        if (!$this->setUser($username, $password)) return false;
+
+
+        //SQL-fråga
+        $sql = "SELECT * FROM user WHERE username='$username'";
+        $result = $this->db->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $stored_password = $row['password'];
+
+            //Kontrollerar det inmatade lösenordet mot det lagrade lösenordet
+            if ($password == $stored_password) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    //Destruktor
+    function __destruct()
+    {
+        mysqli_close($this->db);
+    }
+}
