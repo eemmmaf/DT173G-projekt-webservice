28eee42e13d1c711dc41437376b0b7e057c12cfd
Emma Forslund <emfo2102@student.miun.se>
2022-06-03 00:33:35 +0200

    Lagt till en standard-insert för kategorier och typer

--- install.php
+++ install.php
@@ -47,36 +47,43 @@ $sql .= "DROP TABLE IF EXISTS food_category, drink_category, food_type, drink_ty
 
 $sql .= "CREATE TABLE food_category(
     food_category_id INT(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    food_category_name VARCHAR(128) NOT NULL);
+    food_category_name VARCHAR(128) NOT NULL);";
 
-        CREATE TABLE drink_category(
+$sql .= "CREATE TABLE drink_category(
         drink_category_id INT(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
-        drink_category_name VARCHAR(128) NOT NULL);
-        
-        CREATE TABLE food_type(
+        drink_category_name VARCHAR(128) NOT NULL);";
+
+$sql .= "CREATE TABLE food_type(
         food_type_id INT(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
-        food_type_name VARCHAR(128) NOT NULL);
-        ;";
-
-//Lägger till foreign keys till mat och drink
-
-$sql .=
-    "ALTER TABLE food
-ADD FOREIGN KEY (food_category_id) REFERENCES food_category(food_category_id) ON UPDATE CASCADE;
-
-ALTER TABLE food
-ADD FOREIGN KEY(food_type_id) REFERENCES food_type(food_type_id) ON UPDATE CASCADE;
-
-ALTER TABLE drink
-ADD FOREIGN KEY (drink_category_id) REFERENCES drink_category(drink_category_id) ON UPDATE CASCADE;
-";
+        food_type_name VARCHAR(128) NOT NULL);";
 
 $sql .= "DROP TABLE IF EXISTS tokens, user;";
 
 $sql .= "CREATE TABLE user(
     username VARCHAR(128) NOT NULL PRIMARY KEY,
-    password VARCHAR(128),
-    token VARCHAR(128))";
+    password VARCHAR(128));";
+
+$sql .= "CREATE TABLE tokens(
+        token_id INT(11) NOT NULL PRIMARY KEY,
+        token VARCHAR(128));";
+
+//Lägger till foreign keys till mat och drink
+
+$sql .=
+    "ALTER TABLE food
+ADD FOREIGN KEY (food_category_id) REFERENCES food_category(food_category_id) ON UPDATE CASCADE;";
+
+$sql .= "ALTER TABLE food
+ADD FOREIGN KEY(food_type_id) REFERENCES food_type(food_type_id) ON UPDATE CASCADE;";
+
+$sql .= "ALTER TABLE drink
+ADD FOREIGN KEY (drink_category_id) REFERENCES drink_category(drink_category_id) ON UPDATE CASCADE;
+";
+
+//Gör en insert till kateogierna och typerna så att de hamnar i rätt ordning vid installering
+$sql .= "INSERT INTO food_category (food_category_id, food_category_name) VALUES (1, 'Förrätt'), (2, 'Varmrätt'), (3, 'Efterrätt');";
+$sql .= "INSERT INTO food_type(food_type_id, food_type_name) VALUES (1,'Grill'), (2, 'Pasta'), (3, 'Pizza'), (4, 'Sött'), (5, 'Förrätt');";
+$sql .= "INSERT INTO drink_category (drink_category_id, drink_category_name) VALUES ('1','Vitt vin'), (2, 'Rött vin'), (3, 'Öl'), (4, 'Alkoholfritt');";
 
 
 echo "<pre> $sql </pre>";
