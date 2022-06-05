28d030691a298c972e7833779efc86bb1085688c
Emma Forslund <emfo2102@student.miun.se>
2022-06-05 16:19:53 +0200

    Ändrat sökväg till config

--- includes/settings.php
+++ includes/settings.php
@@ -1,5 +1,5 @@
 <?php
-include_once('config.php');
+include_once('includes/config.php');
 /*Headers med inställningar för din REST webbtjänst*/
 
 //Använder asterisk så att webbtjänsten går att komma åt från alla domäner
