8b772635abf8489abbbc90074bd53e86d0cdbd49
Emma Forslund <emfo2102@student.miun.se>
2022-06-05 11:01:47 +0200

    Skapat en enkel php-sida för registrering så lösenordet blir hashat

---
+++ register.php
@@ -0,0 +1,36 @@
+<?php
+include('config.php');
+session_start();
+
+if (isset($_POST['username'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $register = new User();
+    $success = true; //Variabel för när det postade är OK
+
+    //Anropar setmetoder. Registrerar inte användaren om setMetoderna inte uppfylls
+    if (!$register->setUser($username, $password)) {
+        $success = false;
+    }
+    if ($register->registerUser($username, $password)) {
+        $message = "<p> Användare skapad </p>";
+    }
+}
+?>
+    
+    
+    <form action="register.php" method="POST" id="register">
+        <!--Epost-->
+        <label for="username">Användarnamn:</label><br>
+        <input type="username" name="username" id="username"><br><br>
+        <div class="error-js"></div>
+        <!--Lösenord-->
+        <label for="password">Lösenord:</label><br>
+        <input type="password" name="password" id="password" placeholder="Lösenordet måste innehålla minst 8 tecken"><br><br>
+        <p id="ok"></p>
+
+        <!--Godkänn lagring-->
+        <input type="checkbox" id="approve" name="approve" value="Jag godkänner"">
+        <!--Logga in-->
+        <input type="submit" value="Skapa blogg" id="submitEl">
